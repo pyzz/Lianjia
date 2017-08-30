ef1c7d7642b1a392f8465b7e7e4a76049a4762d6
qshine <qjianxun0@gmail.com>
2017-08-30 15:09:25 +0800

    存储数据库

--- Lianjia/pipelines.py
+++ Lianjia/pipelines.py
@@ -4,8 +4,21 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+from databases.basic_db import engine, session
+from databases.tables import *
 
 class LianjiaPipeline(object):
+    
+    def __init__(self):
+        Base.metadata.create_all(engine)
+        self.session = session
+
     def process_item(self, item, spider):
-        return item
+    	try:
+    		data = Lianjia(**item)
+    		self.session.add(data)
+    	except Exception as e:
+            self.logger.warning(e)
+    	else:
+    		self.session.commit()
+
--- Lianjia/spiders/lianjia.py
+++ Lianjia/spiders/lianjia.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import re
 import scrapy
+from Lianjia.items import LianjiaItem
+from Lianjia.parses import HOUSE_INFO
 
 
 class LianjiaSpider(scrapy.Spider):
@@ -9,41 +11,48 @@ class LianjiaSpider(scrapy.Spider):
     start_urls = ['https://bj.lianjia.com/ershoufang/']
     patt = re.compile(r'/pg\d{0,3}/')
 
+
     def parse(self, response):
         area_infos = response.xpath('/html/body/div[3]/div[1]/dl[2]/dd/div[1]/div/a')
-        for area in area_infos:
+        for area in area_infos[:1]:
         	area_url = response.urljoin(area.xpath('@href').extract_first())
         	area_name = area.xpath('text()').extract_first()
-        	print area_url
         	yield scrapy.Request(
         		area_url, 
-        		priority=2,
         		meta={'area_name': area_name},
         		callback=self.list_page,
+                priority=2,
         	)
 
     def list_page(self, response):
     	home_urls = response.xpath('/html/body/div[4]/div[1]/ul/li/div[1]/div[1]/a/@href').extract()
-    	for url in home_urls:
+    	for url in home_urls[:1]:
     		yield scrapy.Request(
     			response.urljoin(url),
     			meta=response.meta,
-    			callback=self.detail_page
+    			callback=self.detail_page,
+                priority=3,
     		)
+
+        # 翻页
     	all_urls = response.xpath('//a/@href').extract()
     	next_page_urls = [url for url in all_urls if re.search(self.patt, url)]
-    	for next_page in next_page_urls:
+    	for next_page in next_page_urls[:1]:
     		yield scrapy.Request(
     			response.urljoin(next_page),
     			callback=self.list_page,
-    			meta=response.meta
+    			meta=response.meta,
+                priority=4,
     		)
 
     def detail_page(self, response):
-    	area_name = response.meta['area_name']
-    	print '-----------'
-
-
+        res_path = response.xpath
+        item = LianjiaItem()
+        item['area_name'] = response.meta['area_name']
+    	item['page_url'] = response.url
+        for key, patt in HOUSE_INFO.iteritems():
+            item[key] = res_path(patt).extract_first()
+        yield item
 
 
 
